d19db60607a586737f067474fd6b05250869fcbf
dangolan <dangolan@amazon.com>
2019-01-14 16:12:37 +0200

    Get next-web-site API

---
+++ CentralLambdaCode/next_web_site.py
@@ -0,0 +1,13 @@
+import requests
+
+ec2 = boto3.client('ec2')
+def handler(event, context):
+    print("Received getNextSpeedTestWebSite GET request, from user: {}".format(event.user))
+
+    headers = {"Content-Type": "application/json",
+               "Case-Yellow-User": event.user}
+
+    response = requests.post("http://internal-cy-internal-load-balancer-1608404301.eu-central-1.elb.amazonaws.com:9080/central/next-web-site", data={}, headers=headers)
+
+    return response.json
+
