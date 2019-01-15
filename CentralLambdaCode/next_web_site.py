caeb6607a60e4514b7336f696794ece3f49da026
dangolan <dangolan@amazon.com>
2019-01-15 15:42:58 +0200

    Fix header value format

--- CentralLambdaCode/next_web_site.py
+++ CentralLambdaCode/next_web_site.py
@@ -4,7 +4,7 @@ def handler(event, context):
     print("Received getNextSpeedTestWebSite GET request, from user: {}".format(event.get("user")))
 
     headers = {"Content-Type": "application/json",
-               "Case-Yellow-User": event.user}
+               "Case-Yellow-User": event.get("user")}
 
     response = requests.post("http://internal-cy-internal-load-balancer-1608404301.eu-central-1.elb.amazonaws.com:9080/central/next-web-site", data={}, headers=headers)
 
